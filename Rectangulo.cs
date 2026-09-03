31806425c1f0d133b4ead1fcccf422b016d10b3c
MjokHein <dany.f.uribe1404@gmail.com>
2026-09-03 18:46:28 -0500

    Rectangulo
    Daniel Uribe

---
+++ Rectangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebatanaka
+{
+    internal class Rectangulo : Figura
+    {
+        private float baseRectangulo;
+        private float alturaRectangulo;
+        public override float CalcularArea()
+        {
+            return baseRectangulo * alturaRectangulo;
+        }
+    }
+}
