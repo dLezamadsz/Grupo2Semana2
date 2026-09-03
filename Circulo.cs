037ca984b434ffe9ed6851d51843dcf659e73876
D. Lezama <diavalez@gmail.com>
2026-09-03 18:34:34 -0500

    clase círculo

---
+++ Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo2Semana1
+{
+    internal class Circulo : Shape
+    {
+        public float r;
+
+        public Circulo(float r)
+        {
+            this.r = r;
+        }
+
+        public override float GetArea()
+        {
+            return 3.14f * r * r;
+        }
+    }
+}
