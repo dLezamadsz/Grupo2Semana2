a4983f92e23705cc56333d264c5017f039f85a23
Carlosz456 <karloszp001@gmail.com>
2026-09-03 18:54:29 -0500

    Cuadrado
    falta que sea hijo de rectángulo

---
+++ Cuadrado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo2Semana1
+{
+    internal class Cuadrado : Rectangulo 
+    {
+        public float lado;
+        public Cuadrado(float lado)
+        {
+            this.lado = lado;
+        }
+        public override float GetArea()
+        {
+            return lado * lado;
+        }
+    }
+}
