71fc4ecb416795f595f7fbcf34cf89ed389b227b
HoneyVictor22 <melgonzalesvillena22@gmail.com>
2026-09-03 18:25:04 -0600

    sis
    gonzalesvillenajsbdh

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo2Semana2
+{
+    internal class Figura
+    {
+        public virtual float GetArea()
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
